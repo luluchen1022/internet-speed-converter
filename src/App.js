2baea92e280c144407ac07df89a11a5d1051de9e
lulu <luluchen1022@gmail.com>
2021-05-17 01:02:14 +0800

    useState control value

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [inputValue, setInputValue] = useState(0)
   return (
     <div className="container">
       <div className="card-header">Network Speed Converter</div>
@@ -13,11 +15,11 @@ function App() {
         <div className="converter">
           <div>
             <div className="converter-title">Set</div>
-            <div><input type="number" min="0"></input></div>
+            <div><input className="converter-input" type="number" min="0" onChange={(e) => setInputValue(e.target.valNaue)} value={inputValue}></input></div>
           </div>
           <div>
             <div className="converter-title">Show</div>
-            <div><input type="number" min="0"></input></div>
+            <div><input className="converter-input" type="number" min="0" value={inputValue / 8} disabled></input></div>
           </div>
         </div>
       </div>
