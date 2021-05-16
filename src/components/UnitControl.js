186a4dac19272f412793fc964cf1ad478d285a53
lulu <luluchen1022@gmail.com>
2021-05-17 01:20:17 +0800

    add UnitControl component

---
+++ src/components/UnitControl.js
@@ -0,0 +1,8 @@
+export const UnitControl = () => {
+    return (
+        <div className="unit-control">
+            <div className="unit">Mbps</div>
+            <div className="unit">MB/s</div>
+        </div>
+    )
+}
